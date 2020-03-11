25411a66ff6fa5d2344294e3d7b8960393640938
unknown <d93409@gmail.com>
2020-03-11 10:24:22 -0600

    commit

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 require('dotenv').config()
 require('./database');
-
-const express = require('express')
-const cors  = require('cors')
+const { GraphQLServer } = require('graphql-yoga');
 const { makeExecutableSchema } = require("graphql-tools");
 const { importSchema } = require("graphql-import");
 const typeDefs = importSchema("./schema.graphql");
 const resolvers = require('./resolvers');
-const bodyParser = require('body-parser')
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 
 const schema = makeExecutableSchema({
 	typeDefs,
 	resolvers,
 });
 
+const options = {
+    port: 8000,
+    endpoint: '/graphql',
+    subscriptions: '/subscriptions',
+    playground: '/playground',
+};
 
-const PORT = 8000
+const server = new GraphQLServer({
+  schema,
+  context: req => ({...req})
+});
 
-const app = express()
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema }))
-app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
-
-app.listen(PORT)
-
-app.use(cors())
-
-console.log(`Server listening on http://localhost:${PORT} ...`)
+server.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`,
+  ),
+);
