8fd5487e274e38c0f0ee00d330b204d44dca85b2
unknown <d93409@gmail.com>
2020-03-11 14:49:14 -0600

    url

--- index.js
+++ index.js
@@ -5,17 +5,30 @@ const { makeExecutableSchema } = require("graphql-tools");
 const { importSchema } = require("graphql-import");
 const typeDefs = importSchema("./schema.graphql");
 const resolvers = require('./resolvers');
-
+const express = require('express')
 const schema = makeExecutableSchema({
 	typeDefs,
 	resolvers,
 });
+
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
 
 const options = {
     port: 8000,
     endpoint: '/graphql',
     subscriptions: '/subscriptions',
     playground: '/playground',
+    cors: {
+      credentials: true,
+      origin: ["http://localhost:3000"] // your frontend url.
+    }
 };
 
 const server = new GraphQLServer({
