00268fb0642df56b4f56234401a472eaf2212ae9
unknown <d93409@gmail.com>
2020-03-27 11:02:27 -0600

    connection db prueba

--- database/index.js
+++ database/index.js
@@ -28,7 +28,7 @@
 
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var  pool = mysql.createPool({
 
   database:"heroku_e815156f37933c9",
   host: "us-cdbr-iron-east-04.cleardb.net",
@@ -38,26 +38,38 @@ var con = mysql.createConnection({
   
 });
 
+var DB = (function () {
 
-function handleDisconnect() {
+  function _query(query, params, callback) {
+      pool.getConnection(function (err, connection) {
+          if (err) {
+              connection.release();
+              callback(null, err);
+              throw err;
+          }
 
-  con.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }  
-    console.log("Connected!");                                   // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  con.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
+          connection.query(query, params, function (err, rows) {
+              connection.release();
+              if (!err) {
+                  callback(rows);
+              }
+              else {
+                  callback(null, err);
+              }
 
-handleDisconnect();
- module.exports=con;
+          });
+
+          connection.on('error', function (err) {
+              connection.release();
+              callback(null, err);
+              throw err;
+          });
+      });
+  };
+
+  return {
+      query: _query
+  };
+})();
+
+module.exports = DB;
