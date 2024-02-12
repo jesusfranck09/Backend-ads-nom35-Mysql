d1f94003c3c1b5ab3a88824db98cee3dc9f09713
jesusfranck09 <d93409@gmail.com>
2024-02-12 09:50:47 -0600

    update

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 require('dotenv').config()
 require('./database');
-const { GraphQLServer } = require('graphql-yoga');
-const { makeExecutableSchema } = require("graphql-tools");
+const { createYoga,createSchema } = require('graphql-yoga');
 const { importSchema } = require("graphql-import");
 const typeDefs = importSchema("./schema.graphql");
 const resolvers = require('./resolvers');
 const express = require('express')
 const path = require ('path')
-const schema = makeExecutableSchema({
-	typeDefs,
-	resolvers,
-});
+const {createServer } = require( 'node:http')
+
 
 const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
-const options = {
-  endpoint: '/graphql',
-  subscriptions: '/subscriptions',
-  playground: '/playground',
-};
 
-const server = new GraphQLServer({
-  schema,
-  app,
-  context: req => ({...req})
-});
+
+const yoga = createYoga({
+  schema: createSchema({
+    encodeURI:'/graphql',
+    typeDefs: typeDefs,
+    resolvers:resolvers
+  })
+})
+
+const server = createServer(yoga)
 
 const PORT = process.env.PORT || 4000;
 
-server.start(options, () =>
+server.listen(PORT, () =>
   console.log(
     `Server started, listening on port ${PORT} for incoming requests.`,
   ),
 );
+
+
